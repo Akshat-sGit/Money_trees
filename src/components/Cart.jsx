8748f0dbea7b9604138136588a910e7bb71c9edf
Akshat Agarwal <e21cseu0806@bennett.edu.in>
2023-11-23 19:32:07 +0530

    cha

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -18,7 +18,7 @@ const Cart = ({cart,setCart}) => {
         return(
           <>
           <div className="card mb-3 my-5" style={{width:'700px'}}>
-  <div className="row g-0">
+  <div className="row g-0 cart-card">
     <div className="col-md-4">
       <img src={product.imgSrc} className="img-fluid rounded-start" alt="..." />
     </div>
@@ -27,7 +27,7 @@ const Cart = ({cart,setCart}) => {
         <h5 className="card-title">{product.title}</h5>
         <p className="card-text">{product.description}</p>
         <button className="btn btn-primary mx-3">
-                        {product.price} ₹
+                        {product.price} ETH
                       </button>
                       <button
                  
