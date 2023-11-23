c83cbd1f1aea594ad30cdb9573ac4265020610d3
kartikeyasureka <kartikeyasureka@gmail.com>
2023-11-23 21:25:50 +0530

    deployed successfully with a few changes

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -1,4 +1,5 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-verify");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
@@ -7,7 +8,7 @@ module.exports = {
   networks: { 
     sepolia: { 
       url: process.env.PROVIDER_URL, 
-      accounts: [`0x${process.env.PRIVATE_KEY}`]
+      accounts: [process.env.PRIVATE_KEY]
     }
   }
 };
--- scripts/deploy.cjs
+++ scripts/deploy.cjs
@@ -2,13 +2,21 @@ require('dotenv').config();
 const { ethers } = require('hardhat');
 
 async function main() {
+  const { PRIVATE_KEY, PROVIDER_URL } = process.env;
+
+  if (!PRIVATE_KEY || !PROVIDER_URL) {
+    console.error("Please provide PRIVATE_KEY and PROVIDER_URL in your .env file");
+    process.exit(1);
+  }
+
   const [deployer] = await ethers.getSigners();
 
-  console.log('Deploying EcommerceStock to Sepolia test network...');
-  
+  console.log(`Deploying EcommerceStock to Sepolia test network using provider: ${PROVIDER_URL}`);
+
   const EcommerceStock = await ethers.getContractFactory('EcommerceStock');
   const ecommerceStock = await EcommerceStock.deploy();
 
+  // Wait for the contract to be mined
   await ecommerceStock.waitForDeployment();
 
   console.log('EcommerceStock deployed to:', ecommerceStock.address);
